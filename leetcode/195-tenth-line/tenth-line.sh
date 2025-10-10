931fda0f8e852ad0242592cddc4b3ecc0b890626
s3708926 <nil_adhikary@outlook.com>
2025-10-10 10:31:35 +0200

    195-tenth-line solutions

--- leetcode/195-tenth-line/tenth-line.sh
+++ leetcode/195-tenth-line/tenth-line.sh
@@ -1,8 +1,14 @@
-# sol 1
-awk 'NR==10' file.txt
+# PASSED sol 1
+#awk 'NR==10' file.txt
 
-# sol 2
-sed -n '10p' file.txt
+# BEST-CASE PASSED sol 2
+#sed -n '10p' file.txt
 
-# sol 3, inefficient for high n
-head -n 10 file.txt | tail -n 1
+# FAILED sol 3, inefficient for high n
+# head -n 10 file.txt | tail -n 1
+
+# PASSED sol4, like 3 but with handling for <10 lines
+# [ $(wc -l < file.txt) -ge 10 ] && head -n 10 file.txt | tail -n 1
+
+# sol5
+# perl -ne 'print if $. == 10; exit if $. == 10' file.txt
