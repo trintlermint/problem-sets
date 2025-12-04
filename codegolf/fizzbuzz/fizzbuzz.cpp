c04c7f3275d87e8416f3491b2e6bb0f2b0643a96
trintlermint <nil_adhikary@outlook.com>
2025-12-04 02:01:48 +0100

    cg: fizz buzz

---
+++ codegolf/fizzbuzz/fizzbuzz.cpp
@@ -0,0 +1,6 @@
+// 223rd trintlermint 143
+#include<bits/stdc++.h>
+using namespace std;int old(){for(int i=1;i<=100;i++)cout<<(i%3?i%5?to_string(i):"Buzz":i%5?"Fizz":"FizzBuzz")<<'\n';}
+// 103rd trintlermint 111
+#include<cstdio>
+int main(){for(int i=1;i<101;i++)printf(i%3?i%5?"%d\n":"Buzz\n":i%5?"Fizz\n":"FizzBuzz\n",i);}
